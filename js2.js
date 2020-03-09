6501fb83016fff7e1e7c53561875f5125a11840f
Trần Hoàng <tranvhoangnd@example.com>
2020-03-09 22:11:17 +0700

    first commit

---
+++ js2.js
@@ -0,0 +1,12 @@
+function show() {
+
+    var element = document.getElementById("fly");
+    element.classList.toggle("flyout");
+}
+function showmenu() {
+
+    var element = document.getElementById("flymenu");
+    element.classList.toggle("menu-flyout");
+}
+
+
